0fc89014748b07978459e3f72c137c0d8dee5285
Raigm <rai.gordejuela@gmail.com>
2020-04-09 15:49:15 +0200

    debugging test_swap_k.py

--- week2/assignment/checks_a1.py
+++ week2/assignment/checks_a1.py
@@ -35,3 +35,55 @@ k = int(len(L) / 2)
 
 print(a1.swap_k(L, k))
 
+
+
+k = int(len(L) / 2)
+
+
+import a1
+import unittest
+
+
+class TestSwapK(unittest.TestCase):
+    """ Test class for function a1.swap_k. """
+
+    # Add your test methods for a1.swap_k here.
+    def test_swap_k_example1(self):
+        """ Test swap_k with list of even length and k = 2. """
+
+        nums = [1, 2, 3, 4, 5, 6]
+        a1.swap_k(nums, 2)
+        actual = nums
+        expected = [5, 6, 3, 4, 1, 2]
+        self.assertEqual(expected, actual)
+
+    def test_swap_k_example2(self):
+        """ Test swap_k with list of odd length and k = 1."""
+
+        nums = [1, 2, 3, 4, 5]
+        a1.swap_k(nums, 1)
+        actual = nums
+        expected = [5, 2, 3, 4, 1]
+        self.assertEqual(expected, actual)
+
+    def test_swap_k_example3(self):
+        """ Test swap_k with list of int and k == len(L) // 2. """
+
+        nums = [1, 2, 3, 4]
+        a1.swap_k(nums, 2)
+        actual = nums
+        expected = [3, 4, 1, 2]
+        self.assertEqual(expected, actual)
+
+    def test_swap_k_example4(self):
+        """ Test swap_k with list of str. """
+
+        fruits = ['apple', 'banana', 'peach', 'orange']
+        a1.swap_k(fruits, 2)
+        actual = fruits
+        expected = ['peach', 'orange', 'apple', 'banana']
+        self.assertEqual(expected, actual)
+
+if __name__ == '__main__':
+    unittest.main(exit=False)
+
--- week2/assignment/test_swap_k.py
+++ week2/assignment/test_swap_k.py
@@ -10,45 +10,45 @@ class TestSwapK(unittest.TestCase):
 
         L = [1, 2, 3, 4, 5, 6]
         k = 0
-        actual = a1.swap_k(L, k)
+        a1.swap_k(L, k)
         expected = [1, 2, 3, 4, 5, 6]
-        self.assertEqual(actual, expected)
+        self.assertEqual(L, expected)
 
     def test_swap_k_2(self):
         """Test swap_k for k = 1"""
 
         L = [1, 2, 3, 4, 5, 6]
         k = 1
-        actual = a1.swap_k(L, k)
+        a1.swap_k(L, k)
         expected = [6, 2, 3, 4, 5, 1]
-        self.assertEqual(actual, expected)
+        self.assertEqual(L, expected)
 
     def test_swap_k_3(self):
         """Test swap_k for max k """
 
         L = [1, 2, 3, 4, 5, 6]
         k = int(len(L) / 2)
-        actual = a1.swap_k(L, k)
+        a1.swap_k(L, k)
         expected = [4, 5, 6, 1, 2, 3]
-        self.assertEqual(actual, expected)
+        self.assertEqual(L, expected)
 
     def test_swap_k_4(self):
         """Test swap_k for max k and ood len L """
 
         L = [1, 2, 3, 4, 5]
         k = int(len(L) / 2)
-        actual = a1.swap_k(L, k)
+        a1.swap_k(L, k)
         expected = [4, 5, 3, 1, 2]
-        self.assertEqual(actual, expected)
+        self.assertEqual(L, expected)
 
     def test_swap_k_5(self):
         """Test swap_k for max k and list of strings """
 
         L = ["uno", "dos", "tres", "cuatro"]
         k = int(len(L) / 2)
-        actual = a1.swap_k(L, k)
-        expected = ["tres", "cuatro","uno", "dos"]
-        self.assertEqual(actual, expected)
+        a1.swap_k(L, k)
+        expected = ["tres", "cuatro", "uno", "dos"]
+        self.assertEqual(L, expected)
 
 
 if __name__ == '__main__':
